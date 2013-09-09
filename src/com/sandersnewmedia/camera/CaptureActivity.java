a33ca2d2d23a762d446c2246599122ebb978fc97
Abid H. Mujtaba <abid@sandersnewmedia.com>
2013-09-09 14:50:02 -0500

    Adds video preview surface to Capture Activity.

--- src/com/sandersnewmedia/camera/CaptureActivity.java
+++ src/com/sandersnewmedia/camera/CaptureActivity.java
@@ -1,11 +1,165 @@
 package com.sandersnewmedia.camera;
 
-import android.app.Activity;
-import android.os.Bundle;
+// Most of this code is based on the git project: https://github.com/vanevery/Custom-Video-Capture-with-Preview.git
 
-public class CaptureActivity extends Activity {
+//import android.app.Activity;
+//import android.os.Bundle;
+
+import java.io.File;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+
+public class CaptureActivity extends Activity implements SurfaceHolder.Callback {
+
+    public static final String LOGTAG = "CAPTURE";
+
+    private MediaRecorder recorder;
+    private SurfaceHolder holder;
+    private CamcorderProfile camcorderProfile;
+    private Camera camera;
+
+    boolean recording = false;      // Flag used to determine whether the video is being recorded or not.
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        camcorderProfile = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+
         setContentView(R.layout.capture);
+
+        SurfaceView cameraView = (SurfaceView) findViewById(R.id.CameraView);
+        holder = cameraView.getHolder();
+
+        holder.addCallback(this);             // Note: This is the line which causes the emulator app to crash
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+//        cameraView.setClickable(true);
+//        cameraView.setOnClickListener(this);
+    }
+
+//    public void onClick(View v) {
+//        if (recording) {
+//            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+//            recorder.stop();
+//            if (usecamera) {
+//                try {
+//                    camera.reconnect();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            // recorder.release();
+//            recording = false;
+//            Log.v(LOGTAG, "Recording Stopped");
+//            // Let's prepareRecorder so we can record again
+//            prepareRecorder();
+//        } else {
+//            Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+//
+//            recording = true;
+//            recorder.start();
+//            Log.v(LOGTAG, "Recording Started");
+//        }
+//    }
+
+    private void prepareRecorder() {
+        recorder = new MediaRecorder();
+        recorder.setPreviewDisplay(holder.getSurface());
+
+        camera.unlock();
+        recorder.setCamera(camera);
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+
+        recorder.setProfile(camcorderProfile);
+
+        try {
+            File newFile = File.createTempFile("videocapture", ".mp4", Environment.getExternalStorageDirectory());
+            recorder.setOutputFile(newFile.getAbsolutePath());
+        } catch (IOException e) {
+            Log.w(LOGTAG,"Couldn't create file");
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(LOGTAG, "surfaceCreated");
+
+        camera = Camera.open();
+
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }
+        catch (IOException e) {
+            Log.e(LOGTAG,e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d(LOGTAG, "surfaceChanged");
+
+        if (!recording) {
+                camera.stopPreview();
+
+            try {
+                Camera.Parameters p = camera.getParameters();
+
+                p.setPreviewSize(camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
+                p.setPreviewFrameRate(camcorderProfile.videoFrameRate);
+
+                camera.setParameters(p);
+
+                camera.setPreviewDisplay(holder);
+                camera.startPreview();
+            }
+            catch (IOException e) {
+                Log.e(LOGTAG,e.getMessage());
+                e.printStackTrace();
+            }
+
+            prepareRecorder();
+        }
+    }
+
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+        Log.v(LOGTAG, "surfaceDestroyed");
+
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+
+        recorder.release();
+        camera.release();
+
+        finish();
     }
 }
