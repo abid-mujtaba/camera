abf3f478905de9a49f205cff355c2337bbc7bda5
Abid H. Mujtaba <abid@sandersnewmedia.com>
2013-09-09 11:51:15 -0500

    Skeletal Hello World Android project established.

---
+++ src/com/sandersnewmedia/camera/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.sandersnewmedia.camera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntroActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+    }
+}
